be888763b6559d0555e11f2bf26df1e0dbfebc3f
Bhargav Dungrani <146700207+Haridungrani@users.noreply.github.com>
2025-05-02 00:16:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 app.get("/api/testing", (req, res) => {
 	return res.json({
 		success:true,
-		message:'Your server is up and running and this is bhagukheifgwifgfhwfjgw...'
+		message:'Your server is up and running and this is bhagukheifgwifgfhwfjgww...'
 	});
 });
 
